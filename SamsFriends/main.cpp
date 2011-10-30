409a5fceebba9efd16991d1779560adb69f31fd7
herb143 <h.herbie@gmail.com>
2011-10-30 15:12:11 -0400

    Initial Commit

---
+++ SamsFriends/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SamsFriends
+//
+//  Created by Herbert Gilman on 10/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
